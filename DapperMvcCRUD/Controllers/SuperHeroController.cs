be3af22960027ae226e088e3451093a1f13e692c
joseFastyDev <jose.rangel@fastydev.com>
2022-10-14 14:19:03 -0500

    first commit

---
+++ DapperMvcCRUD/Controllers/SuperHeroController.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using DapperMvcCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace DapperMvcCRUD.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public SuperHeroController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        //Http Get Index
+        public async Task<IActionResult> IndexAsync()
+        {
+            using var connection = new NpgsqlConnection(_config.GetConnectionString("DefaultConnection"));
+            var heroes = await connection.QueryAsync<SuperHero>("select * from \"superheroes\"");
+            IEnumerable<SuperHero> listHeroes = heroes.ToList();
+            return View(listHeroes);
+        }
+
+        //Http Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Http Post Create
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(SuperHero hero)
+        {
+            if (ModelState.IsValid)
+            {
+                using var connection = new NpgsqlConnection(_config.GetConnectionString("DefaultConnection"));
+                await connection.ExecuteAsync("insert into \"superheroes\"(" +
+                    "name, firstname, lastname, place) values (@Name, @FirstName, @LastName, @Place)", hero);
+            }
+            return View(hero);
+        }
+    }
+}
---
+++ DapperMvcCRUD/Models/SuperHero.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DapperMvcCRUD.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y máximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y máximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Nombre")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [StringLength(50, ErrorMessage = "El {0} debe ser al menos {2} y máximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Apellido")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "El lugar es obligatorio")]
+        [Display(Name = "Lugar")]
+        public string Place { get; set; }
+    }
+}
